e225e8f43f7d635a7bcc7e6470762b362a9ff7d3
Haridev <haridev.8281@outlook.com>
2024-07-27 10:38:34 +0530

    created user model

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.CONN_STRING).then(console.log("Connected to DB"));
 
 app.listen(7878, () => {
   console.log("Server is listening at http://localhost:7878");
